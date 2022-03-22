53d0dbc87908d99cf7708cfe91ba4c4c56abaf99
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-03-21 23:16:44 -0300

    Criação das classes dos objetos e da Fábrica

---
+++ src/main/java/Posicao.java
@@ -0,0 +1,18 @@
+public class Posicao {
+
+    private String nome;
+    private String areaAtuacao;
+
+    public Posicao(String nome, String areaAtuacao) {
+        this.nome = nome;
+        this.areaAtuacao = areaAtuacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAreaAtuacao() {
+        return areaAtuacao;
+    }
+}
